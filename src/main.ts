ed05f9a0ad03129878938cbba31f36079ed29666
ihorhnennyi <ihorhnennyi@gmail.com>
2025-03-27 09:06:03 +0200

    APP 1.1

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://workriseup.website'],
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
     methods: 'GET, POST, PATCH, DELETE',
